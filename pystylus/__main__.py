37fa010d2e3624d8455079f94d60f382972b0d33
Andrew Kubera <andrewkubera@gmail.com>
2015-04-19 16:51:58 +0200

    Added some program options to __main__

--- pystylus/__main__.py
+++ pystylus/__main__.py
@@ -9,10 +9,25 @@ from argparse import ArgumentParser
 
 def parse_args(args):
     parser = ArgumentParser("pystylus")
+    parser.add_argument("filenames",
+                        nargs='*',
+                        type=str,
+                        help="Filename(s) which to convert to css"
+                        )
+    parser.add_argument("--",
+                        dest='read_from_stdin',
+                        action='store_true',
+                        help="Read from STDIN instead of file."
+                        )
     return parser.parse_args(args)
 
 def main(args):
     args = parse_args(args)
+
+    if args.filenames:
+        print(args.filenames)
+    elif args.read_from_stdin:
+        print("Reading from stdin...")
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
