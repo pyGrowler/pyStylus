e059e7265d5452b47f544751564d42f8f4bb1134
Andrew Kubera <andrewkubera@gmail.com>
2015-08-22 23:55:19 -0400

    Added an environment submodule which will manage pystylus functions and variables

---
+++ pystylus/environment/__init__.py
@@ -0,0 +1,9 @@
+#
+# pystylus/environment/__init__.py
+#
+"""
+Pystylus module for providing an environment to house and execute the variables
+and functions defined in the stylus scripts.
+"""
+
+pass
